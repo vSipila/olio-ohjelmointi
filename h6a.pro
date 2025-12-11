f7842a345220f575032a13850ba98b42647f8218
vSipila <vsipila24@students.oamk.fi>
2025-12-11 03:47:11 +0200

    Add files via upload

---
+++ h6a.pro
@@ -0,0 +1,11 @@
+TEMPLATE = app
+QT = core
+CONFIG += console c++17
+CONFIG -= app_bundle
+
+SOURCES += \
+        main.cpp \
+        myclass.cpp
+
+HEADERS += \
+    myclass.h
---
+++ myclass.cpp
@@ -0,0 +1,19 @@
+#include "myclass.h"
+
+MyClass::MyClass(QObject *parent) : QObject(parent)
+{
+    // vanha syntaksi:
+    // connect(this, SIGNAL(mySignal()), this, SLOT(mySlot()));
+    // uusi syntaksi:
+    connect(this, &MyClass::mySignal, this, &MyClass::mySlot);
+}
+
+void MyClass::raiseMySignal()
+{
+    emit mySignal();
+}
+
+void MyClass::mySlot()
+{
+    qDebug() << "mySlot:ia kutsuttiin";
+}
